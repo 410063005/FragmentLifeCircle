6d33c42f1645d729a8ba914d6436ecbf5e3d48ea
chenming <410063005@163.com>
2018-07-19 17:32:36 +0800

    mod

--- demo-glide/src/main/java/com/example/demo_glide/MainActivity.kt
+++ demo-glide/src/main/java/com/example/demo_glide/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.demo_glide
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonPNG.setOnClickListener {
+            PNGActivity.start(this, "http://igame-10037599.image.myqcloud.com/b8153781-3a33-4d0b-be09-a0e5063047c7")
+        }
+        buttonWebp.setOnClickListener {
+            WebpActivity.start(this, "http://igame-10037599.image.myqcloud.com/b8153781-3a33-4d0b-be09-a0e5063047c7?imageView2/format/webp")
+        }
     }
 }
---
+++ demo-glide/src/main/java/com/example/demo_glide/WebpActivity.java
@@ -0,0 +1,34 @@
+package com.example.demo_glide;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
+
+public class WebpActivity extends AppCompatActivity {
+
+    public static void start(Context context, String url) {
+        Intent starter = new Intent(context, WebpActivity.class);
+        starter.putExtra("url", url);
+        context.startActivity(starter);
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_from_qcloud);
+
+        String url = getIntent().getStringExtra("url");
+
+        ImageView imageView = findViewById(R.id.imageView);
+        Glide.with(this).load(url).apply(
+                new RequestOptions().skipMemoryCache(true).diskCacheStrategy(DiskCacheStrategy.NONE))
+                .into(imageView);
+    }
+}
